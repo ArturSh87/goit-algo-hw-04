02a52e123ff863dc19d41bc0c2e86f30b18427e7
artur <ukdknd@gmail.com>
2025-06-05 17:26:26 +0300

    Update

--- total_salary/main.py
+++ total_salary/main.py
@@ -1,30 +1,19 @@
 from pathlib import Path
 
-file_name = Path.home() / 'PythonProjects' / 'HomeWork' / 'goit-algo-hw-04' / 'total_salary'
-
-try:
-    with open(file_name / 'salaries.txt', 'r', encoding='utf-8') as file:
-        for line in file:
-            print(line.strip().split(',')) 
-except FileNotFoundError:
-    print("File not found")
-
+file_name = Path.home() / 'PythonProjects' / 'HomeWork' / 'goit-algo-hw-04' / 'total_salary' / 'salaries.txt'
 def total_salary(path):
-    
     try:
-        with open(file_name / 'salaries.txt', 'r', encoding='utf-8') as file:
+        with open(path, 'r', encoding='utf-8') as file:
             total = 0
             count = 0
             for line in file:
                 name, salary = line.strip().split(',')
                 total += int(salary)
                 count += 1
-            average = total / count if count else 0
+            average = int(total / count) if count else 0
             return total, average
     except FileNotFoundError:
         print('File no found')
         return 0, 0
-file_path = Path.home() / 'PythonProjects' / 'HomeWork' / 'goit-algo-hw-04' / 'total_salary'
-total, average = total_salary(file_path)
-
+total, average = total_salary(file_name)
 print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")        
